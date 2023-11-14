3b4039792d028e04d5d0d1cfd02ab08ef5f540a1
Doyle Brown <dalpiad@gmail.com>
2023-11-13 19:52:52 -0600

    moving to step 2 of studio

---
+++ part1/studio/studio/src/components/Description.jsx
@@ -0,0 +1,31 @@
+import styles from './Description.module.css';
+import React from 'react';
+
+function RecipeAuthor () {
+    let authorLink = "https://www.pinterest.com/pin/516084438582305490/";
+    let authorPhoto = "https://i.pinimg.com/280x280_RS/31/96/56/3196569849eab9e739a39c9134fc6cde.jpg";
+    let authorName = "Kristen Whitby";
+    
+    return  (
+        <div className = {styles.recipeAuthorBlock}>
+            <img src={authorPhoto} alt = "Author Kristen Whitby" className={styles.imageUpdates} />
+        <div>
+           <h3>{authorName}</h3>
+           <a href={authorLink}>Recipe from ellaclaireinspired.com</a> 
+        </div>
+        </div>
+    )};
+
+class RecipeDescription extends React.Component {
+    render() {
+        <div> 
+            <div>
+                <h1>Butter Pecan Snowball Meltaways</h1>
+                <p>The BEST Snowball Cookies for Christmas</p>
+            </div>
+        <RecipeAuthor />
+        </div>
+    }
+};
+
+export default RecipeDescription;
