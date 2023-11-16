c4889b431608d4095191e1879810f270ca23c9f8
Doyle Brown <dalpiad@gmail.com>
2023-11-15 19:20:05 -0600

    Completed part 2 exercise

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -2,8 +2,12 @@ import './styles.css';
 
 function Button() {
 
+   function onLearnMore() {
+      alert("I'm on a boat!!! I got my flippy floppies");
+   }
+
    return ( 
-         <button>
+         <button onClick={onLearnMore}>
             Learn More
          </button>
    );
